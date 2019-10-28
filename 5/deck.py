5ecd62cd3c42da915578e0a6a1cb4116178f94a6
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-10-28 11:28:41 +0000

    Update deck.py

--- 5/deck.py
+++ 5/deck.py
@@ -1,3 +1,5 @@
+import random
+
 cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 cardSuits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 
@@ -5,4 +7,24 @@ class Deck:
   def __init__(self):
     self.shuffle()
   def shuffle(self):
+    deck = []
+    for number in cardNumbers:
+      for suit in cardSuits:
+        deck.append({number: str(number), suit: str(suit)})
+    self.deck = random.shuffle(deck)
+    self.discards = []
+  def draw(self):
+    try:
+      card = deck.pop()
+      discards.append(card)
+      return card
+    except IndexError:
+      return None
+  def get(self, index):
+    try:
+      return self.deck[-(index+1)]
+    except IndexError:
+      return None
+  def get_discards(self):
+    return self.discards
     
